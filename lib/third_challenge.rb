550851758de567f17e253e0bfbe480466fedf6c3
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-11 18:22:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,16 +15,7 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [
-          {
-            :name => "Benvolio",
-            :age => "17",
-            :attitude => "worried"
-          },
-          {
-            :name => "Mercutio",
-            :age => "18",
-            :attitude => "hot-headed"
-          }
+          
         ]
    }, 
    :capulet => {
@@ -42,16 +33,7 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-          {
-            :name => "Steven",
-            :age => "30",
-            :attitude => "confused"
-          },
-          {
-            :name => "Nurse",
-            :age => "44",
-            :attitude => "worried"
-          }
+         
         ]
    }
   }
